f534080e14c82e46ce5bc139795d3d75ed28a4a8
Daniel Weiß <wanieldeiss@gmail.com>
2020-02-13 23:04:39 +0100

    change default get answer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIndex() {
+    return {
+      message: 'Server is running',
+      hasToken: !!process.env.TOKEN,
+      hasCollectionUrl: !!process.env.COLLECTIONURL,
+    };
   }
 }
