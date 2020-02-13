56e4bb913588c874520f6c27e749289122ae354d
Daniel Weiß <wanieldeiss@gmail.com>
2020-02-13 23:06:26 +0100

    update service with dummy content for azure devops

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { getPersonalAccessTokenHandler, WebApi } from 'azure-devops-node-api';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  azureDevOps() {
+    const collectionURL = process.env.COLLECTIONURL;
+    const token = process.env.TOKEN;
+
+    var authHandler = getPersonalAccessTokenHandler(token);
+    var connection = new WebApi(collectionURL, authHandler);
+
+    var vstsGit = connection.getGitApi().then(
+      success => {
+        console.log(success);
+      },
+      error => {
+        console.log(error);
+      },
+    );
   }
 }
